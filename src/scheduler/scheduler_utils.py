a076bc5790ec0dd2bc38d0ee8de3d118d959e435
Christopher Luciuk <christopherluciuk@Christophers-MacBook-Air.local>
2025-11-01 17:43:44 -0400

    schedule in progress

--- src/scheduler/scheduler_utils.py
+++ src/scheduler/scheduler_utils.py
@@ -1,5 +1,6 @@
 from typing import Dict, List
 
+import numpy as np
 import pandas as pd
 
 
@@ -9,7 +10,31 @@ def _build_schedule(file_path: str, schedule: List[Dict], rules: List[Dict]):
     # We want to make sure we deduplicate against the uploads
     # We want to make sure every row has an upload
     df = pd.DataFrame(schedule)
-    df.to_csv(file_path, index=False)
+    # Fill dates
+    df["date"] = pd.to_datetime(df["date"])
+    if df["date"].isna().all():
+        current_max = pd.Timestamp.today()
+    else:
+        current_max = df["date"].max(skipna=True)
+    for i in range(len(df)):
+        if pd.isna(df.at[i, "date"]):
+            current_max += pd.Timedelta(days=1)
+            df.at[i, "date"] = current_max
+    # Define conditions
+    df["days_diff"] = (df["date"] - pd.Timestamp.today()).dt.days
+    conditions = [
+        df["days_diff"] < 1,
+        (df["days_diff"] >= 1) & (df["days_diff"] <= 7),
+        df["days_diff"] > 7,
+    ]
+
+    # Corresponding values for each condition
+    values = ["Next Day!", "Coming Soon.", "A Long Way Off..."]
+
+    # Default value if none of the conditions match
+    df["status"] = np.select(conditions, values, default="A Long Way Off...")
+    print(df)
+    df[["path", "text", "date", "status"]].to_csv(file_path, index=False)
     return
 
 
