61398727c11b275226f9b207f9ab44f848577ea5
Christopher Luciuk <christopherluciuk@Christophers-MacBook-Air.local>
2025-11-02 13:30:26 -0500

    adding rule to make all posts happen at 10am ET

--- src/scheduler/scheduler_utils.py
+++ src/scheduler/scheduler_utils.py
@@ -2,6 +2,7 @@ from typing import Dict, List
 
 import numpy as np
 import pandas as pd
+import pytz
 
 
 def _build_schedule(file_path: str, schedule: List[Dict], rules: List[Dict]):
@@ -20,6 +21,11 @@ def _build_schedule(file_path: str, schedule: List[Dict], rules: List[Dict]):
         if pd.isna(df.at[i, "date"]):
             current_max += pd.Timedelta(days=1)
             df.at[i, "date"] = current_max
+    # Overwrite the hour in the day
+    est = pytz.timezone("US/Eastern")
+    df["date"] = df["date"].apply(
+        lambda x: x.astimezone(est).replace(hour=10, minute=0, second=0, microsecond=0)
+    )
     # Define conditions
     df["days_diff"] = (df["date"] - pd.Timestamp.today()).dt.days
     conditions = [
